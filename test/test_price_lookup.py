dacb792d9bbb9d714fce8ff68f9f3f4eea9bb3bb
Kenneth Ros <zhaho.ros@gmail.com>
2024-06-07 15:51:13 +0200

    Test

--- test/test_price_lookup.py
+++
@@ -1,8 +0,0 @@
-from lookup import price_lookup
-game = "Las Navas de Tolosa, 1212"
-print("Price Lookup Game: "+game)
-print("Price Lookup: "+str(price_lookup.game_title(game)))
-
-
-def test_price_lookup_game_title():
-        assert type(price_lookup.game_title(game)) == int
